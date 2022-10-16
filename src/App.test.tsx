f9876c091483374e5efcb4f6f58f6995d2a45cb2
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-16 10:12:09 -0300

    test: increase coverage for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
 import { App } from './App';
+import { UsersProvider } from './contexts/UsersContext';
 
 describe('<App />', () => {
 	it('Should render it', () => {
-		const { container } = render(<App />);
+		const { container } = render(
+			<UsersProvider>
+				<App />
+			</UsersProvider>
+		);
 
 		expect(container).toBeInTheDocument();
 	});
