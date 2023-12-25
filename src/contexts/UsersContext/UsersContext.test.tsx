e32c608eba07117bb5f0c89212c79a665d7ce08d
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-12-25 19:11:45 -0300

    test: add missing test for the users context

--- src/contexts/UsersContext/UsersContext.test.tsx
+++ src/contexts/UsersContext/UsersContext.test.tsx
@@ -49,8 +49,17 @@ function TestingComponent() {
 }
 
 describe('<UsersContext />', () => {
+	it('Should be able to render without crashing', () => {
+		const { container } = render(
+			<UsersProvider>
+				<TestingComponent />
+			</UsersProvider>
+		);
 
-	it('Should add a new user after calling the addUser function', async () => {
+		expect(container).toBeInTheDocument();
+	});
+
+	it('Should be able to add a new user after calling the addUser function', async () => {
 		const user = userEvent.setup();
 		const { container } = render(
 			<UsersProvider>
@@ -70,13 +79,28 @@ describe('<UsersContext />', () => {
 		expect(usersList).toContainElement(usersList.querySelector('li'));
 	});
 
-	it.skip('Should remove a specific user after calling removeUser function', () => {
-		const { debug } = render(
+	it('Should be able to remove a specific user after calling removeUser function', async () => {
+		const user = userEvent.setup();
+		const { container } = render(
 			<UsersProvider>
 				<TestingComponent />
 			</UsersProvider>
 		);
 
-		debug();
+		const form = container.querySelector('form') as HTMLFormElement;
+		const input = form.querySelector('input') as HTMLInputElement;
+		const submitButton = form.querySelector('button') as HTMLButtonElement;
+		const usersList = screen.getByTestId('users-list');
+
+		await user.type(input, 'user test');
+		await user.click(submitButton);
+
+		const userToRemove = usersList.querySelector('li') as HTMLElement;
+
+		expect(usersList).toContainElement(userToRemove);
+
+		await user.click(userToRemove.querySelector('button') as HTMLButtonElement);
+
+		expect(usersList).not.toContainElement(userToRemove);
 	});
 });
